cc6995b2fd5fd422d141d833c1c699a07bc259a5
FaziletKosure <faziletkosure1@gmail.com>
2021-03-31 18:48:28 +0200

    created aboutcharter component

---
+++ src/components/AboutCharter.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+const AboutCharter = () => {
+  return (
+    <div className="container">
+      <div className="charter-header">
+        <img src="" alt="" />
+        <div className="charter-header-text py-3">
+          <h2>MUna . io</h2>
+          <h2>Charter</h2>
+        </div>
+      </div>
+      <div className="charter-body-text py-3">
+        <h2>
+          <span>Human-centric</span>
+        </h2>
+        <p>
+          Individual interests come first over commercial gain. We build and use
+          human-centric tools to improve the well-being of our communities.
+        </p>
+      </div>
+      <div className="charter-body-text  py-3">
+        <h2>
+          <span>Equality</span>
+        </h2>
+        <p>
+          Our differences are our utmost strength without any compromise. We
+          strive to ensure that our dataset represents the diversity that is our
+          world.
+        </p>
+      </div>
+      <div className="charter-body-text  py-3">
+        <h2>
+          <span>Control</span>
+        </h2>
+        <p>
+          Individuals have a genuine choice to decide if and how their data is
+          processed, used, and when it is shared with third parties.
+        </p>
+      </div>
+      <div className="charter-body-text  py-3">
+        <h2>
+          <span>Transparent</span>
+        </h2>
+        <p>
+          We make sure that individuals understand why we collect and handle
+          personal information, and only collect data that is necessary. When we
+          use artificial intelligence to make automated decisions, we make sure
+          that the algorithms are explainable and fair.
+        </p>
+      </div>
+      <div className="charter-body-text  py-3">
+        <h2>
+          <span>Accountable</span>
+        </h2>
+        <p>
+          We are accountable for the personal information we handle throughout
+          our ecosystem. We vouch to protect and uphold the trust relationship
+          between the company and its customers.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutCharter;
--- src/secreens/About.js
+++ src/secreens/About.js
@@ -1,21 +1,22 @@
-import React from 'react'
-import useDocumentTitle from '../components/useDocumentTitle'
-import AboutPanel1 from '../components/AboutPanel1'
-import AboutPanel2 from '../components/AboutPanel2'
-import AboutPanel3 from '../components/AboutPanel3'
-import AboutPanel4 from '../components/AboutPanel4'
-
+import React from "react";
+import useDocumentTitle from "../components/useDocumentTitle";
+import AboutPanel1 from "../components/AboutPanel1";
+import AboutPanel2 from "../components/AboutPanel2";
+import AboutPanel3 from "../components/AboutPanel3";
+import AboutPanel4 from "../components/AboutPanel4";
+import AboutCharter from "../components/AboutCharter";
 
 const About = () => {
-    useDocumentTitle('About | Muna.io')
-    return (
-        <>
-         <AboutPanel1/>   
-         <AboutPanel2/>   
-         <AboutPanel3/>   
-         <AboutPanel4/>   
-        </>
-    )
-}
+  useDocumentTitle("About | Muna.io");
+  return (
+    <>
+      <AboutPanel1 />
+      <AboutPanel2 />
+      <AboutCharter />
+      <AboutPanel3 />
+      <AboutPanel4 />
+    </>
+  );
+};
 
-export default About
+export default About;
